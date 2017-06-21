d884631874ee95022894608fd57d1fdf23bae111
Ezequiel Leonardo Castaño <elcg@gmx.com>
2017-06-21 00:02:11 -0300

    Basic MySQL Connection
    MySQL connector

--- ECMM-Reservas/src/data/FactoryConection.java
+++ ECMM-Reservas/src/data/FactoryConection.java
@@ -1,5 +1,47 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class FactoryConection {
+	
+	private static FactoryConection instancia;
+	
+	private Connection conn;
+	
+	private FactoryConection(){
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			}
+	}
+	
+	public static FactoryConection getInstancia(){
+		
+		if (FactoryConection.instancia==null){
+			FactoryConection.instancia=new FactoryConection();
+			
+			}
+		
+		return FactoryConection.instancia;
+		
+	}
+	
+	public Connection getConn(){
+		try {
+			
+			conn=  DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net:3306/sql10181254?user=sql10181254&password=eZkGdZyJe8");
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+			
+		}
+		return conn;		
+	}
 
 }
