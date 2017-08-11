da0c6fd64a368009c65f3536267bc8efc077f31f
Matias-Mulassi <MulassiMatias@gmail.com>
2017-08-11 12:46:03 -0300

    improve UI desktop

--- ECMM-Reservas/src/ui/desktop/MainAppWindow.java
+++ ECMM-Reservas/src/ui/desktop/MainAppWindow.java
@@ -14,11 +14,33 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.SwingConstants;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class MainAppWindow extends JFrame {
 
-	JFrame frmAbmPerson;
-	private JMenuItem mntmAbmc;
+	JFrame frmFinalAssigment;
+	private JMenuItem mntmAbmcPerson;
+	private JDesktopPane desktopPane1;
+	private JMenu mnTypeBookable;
+	private JMenuItem mntmAbmcTypeBookable;
+	private JMenu mnLogIn;
+	private JMenu mnLogOut;
+	private JMenuItem mntmAbmcBookable;
+	private JMenuItem mntmCreateBooking;
+	private JMenuItem mntmShowAllBooking;
+	private JMenuItem mntmDeleteBooking;
+	private JMenuItem mntmShowAllPerson;
+	private JMenuItem mntmModifyBoking;
 	private JDesktopPane desktopPane;
 
 	/**
@@ -29,7 +51,8 @@ public class MainAppWindow extends JFrame {
 			public void run() {
 				try {
 					MainAppWindow window = new MainAppWindow();
-					window.frmAbmPerson.setVisible(true);
+					window.setMinimumSize(new Dimension(550,550));
+					window.frmFinalAssigment.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -48,39 +71,118 @@ public class MainAppWindow extends JFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmAbmPerson = new JFrame();
-		frmAbmPerson.setTitle("Final Assignment Java2017");
-		frmAbmPerson.setBounds(100, 100, 641, 428);
-		frmAbmPerson.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmAbmPerson.getContentPane().setLayout(new BorderLayout(0, 0));
+		frmFinalAssigment = new JFrame();
+		frmFinalAssigment.setTitle("Final Assignment Java2017");
+		frmFinalAssigment.setBounds(100, 100, 550, 428);
+		frmFinalAssigment.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmFinalAssigment.getContentPane().setLayout(new BoxLayout(frmFinalAssigment.getContentPane(), BoxLayout.X_AXIS));
 		
-		desktopPane = new JDesktopPane();
-		desktopPane.setBackground(Color.WHITE);
-		frmAbmPerson.getContentPane().add(desktopPane, BorderLayout.CENTER);
-		desktopPane.setLayout(null);
+		desktopPane1 = new JDesktopPane();
+		desktopPane1.setBackground(Color.WHITE);
+		frmFinalAssigment.getContentPane().add(desktopPane1);
+		desktopPane1.setLayout(new BorderLayout(0, 0));
 		
 		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(0, 0, 97, 21);
-		desktopPane.add(menuBar);
+		menuBar.setBorderPainted(false);
+		desktopPane1.add(menuBar, BorderLayout.NORTH);
 		
-		JMenu mnas = new JMenu("Person");
-		menuBar.add(mnas);
+		JMenu mnPerson = new JMenu("Person");
+		menuBar.add(mnPerson);
 		
-		mntmAbmc = new JMenuItem("ABMC");
-		mntmAbmc.addActionListener(new ActionListener() {
+		mntmAbmcPerson = new JMenuItem("CRUD");
+		mntmAbmcPerson.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			
 			showABMCPerson();
 			
 			}
 		});
-		mnas.add(mntmAbmc);
+		mnPerson.add(mntmAbmcPerson);
 		
-		JDesktopPane desktopPane_1 = new JDesktopPane();
-		desktopPane_1.setBounds(287, 157, 1, 1);
-		desktopPane.add(desktopPane_1);
+		mntmShowAllPerson = new JMenuItem("Show All");
+		mnPerson.add(mntmShowAllPerson);
+		
+		mnTypeBookable = new JMenu("Type Bookable");
+		menuBar.add(mnTypeBookable);
+		
+		mntmAbmcTypeBookable = new JMenuItem("CRUD");
+		mntmAbmcTypeBookable.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+			showABMCTypeBookable();
+			}
+		});
+		mnTypeBookable.add(mntmAbmcTypeBookable);
+		
+		JMenu mnBookable = new JMenu("Bookable");
+		menuBar.add(mnBookable);
+		
+		mntmAbmcBookable = new JMenuItem("CRUD");
+		mnBookable.add(mntmAbmcBookable);
+		
+		JMenu mnBooking = new JMenu("Booking");
+		menuBar.add(mnBooking);
+		
+		mntmCreateBooking = new JMenuItem("Create");
+		mnBooking.add(mntmCreateBooking);
+		
+		mntmModifyBoking = new JMenuItem("Modify");
+		mnBooking.add(mntmModifyBoking);
+		
+		mntmShowAllBooking = new JMenuItem("Show All");
+		mnBooking.add(mntmShowAllBooking);
+		
+		Component verticalStrut = Box.createVerticalStrut(10);
+		mnBooking.add(verticalStrut);
+		
+		mntmDeleteBooking = new JMenuItem("Delete");
+		mnBooking.add(mntmDeleteBooking);
+		
+		mnLogIn = new JMenu("Log In");
+		mnLogIn.setHorizontalAlignment(SwingConstants.RIGHT);
+		menuBar.add(Box.createHorizontalGlue());
+		menuBar.add(mnLogIn);
+		
+		mnLogOut = new JMenu("Log Out");
+		menuBar.add(mnLogOut);
+		
+		desktopPane = new JDesktopPane();
+		desktopPane.setBackground(Color.WHITE);
+		desktopPane1.add(desktopPane, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setHgap(20);
+		panel.setBackground(Color.BLACK);
+		desktopPane1.add(panel, BorderLayout.SOUTH);
+		
+		JLabel lblTpJava = new JLabel("TP Java 2017");
+		lblTpJava.setHorizontalAlignment(SwingConstants.LEFT);
+		lblTpJava.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblTpJava.setForeground(Color.WHITE);
+		panel.add(lblTpJava);
+		
+		JLabel lblCastaoEzequiel = new JLabel("Casta\u00F1o Ezequiel - Mulassi Mat\u00EDas - Sacchi Priscila");
+		lblCastaoEzequiel.setHorizontalAlignment(SwingConstants.LEFT);
+		lblCastaoEzequiel.setForeground(Color.WHITE);
+		lblCastaoEzequiel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		panel.add(lblCastaoEzequiel);
+		
+		JLabel lblDasdf = new JLabel("Comisi\u00F3n 3E02");
+		lblDasdf.setHorizontalAlignment(SwingConstants.LEFT);
+		lblDasdf.setForeground(Color.WHITE);
+		lblDasdf.setFont(new Font("Tahoma", Font.BOLD, 11));
+		panel.add(lblDasdf);
 	}
 
+
+		protected void showABMCTypeBookable() {
+		// TODO Auto-generated method stub
+
+			ABMCType_Bookable frmTypeBookable=new ABMCType_Bookable();
+			desktopPane.add(frmTypeBookable);
+			frmTypeBookable.setVisible(true);
+	}
 
 		protected void showABMCPerson() {
 			
@@ -91,12 +193,5 @@ public class MainAppWindow extends JFrame {
 			
 			
 		}
-
-
-
-
-
-
-
 }
 
